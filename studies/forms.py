1a593491cd5106c5631d0c02be1e9ead4b8f4519
Ajay_kumar_dev <127192236+Ajaykumar-dev-07@users.noreply.github.com>
2024-10-07 23:18:00 +0530

    first commit uploaded files

---
+++ studies/forms.py
@@ -0,0 +1,8 @@
+from . models import Study
+from django import forms
+
+
+class StudyForm(forms.ModelForm):
+    class Meta:
+        model = Study
+        fields = ['study_name', 'study_description', 'study_phase', 'sponsor_name']
